037215e3d05596307ea958cac0c27af910083b9f
Adir Hudayfi <adirh3@gmail.com>
2022-05-04 22:01:57 +0300

    0.10.139993-blast

--- src/Avalonia.Controls.DataGrid/DataGrid.cs
+++ src/Avalonia.Controls.DataGrid/DataGrid.cs
@@ -2059,15 +2059,6 @@ namespace Avalonia.Controls
             }
         }
         
-        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnAttachedToVisualTree(e);
-            if (DataConnection.DataSource != null && !DataConnection.EventsWired)
-            {
-                DataConnection.WireEvents(DataConnection.DataSource);
-            }
-        }
-
         protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
         {
             base.OnDetachedFromVisualTree(e);
@@ -2083,13 +2074,6 @@ namespace Avalonia.Controls
             base.OnAttachedToVisualTree(e);
             if (DataConnection.DataSource != null && !DataConnection.EventsWired)
                 DataConnection.WireEvents(DataConnection.DataSource);
-        }
-
-        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
-        {
-            base.OnDetachedFromVisualTree(e);
-            if (DataConnection.DataSource != null && DataConnection.EventsWired)
-                DataConnection.UnWireEvents(DataConnection.DataSource);
         }
 
         /// <summary>
