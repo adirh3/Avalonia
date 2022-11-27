76d036a5ddae114daf72e45c08a90afa4da5628e
Adir Hudayfi <adirh3@gmail.com>
2022-11-27 20:34:11 +0200

    Revert

--- src/Avalonia.Base/Visual.cs
+++ src/Avalonia.Base/Visual.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections;
 using System.Collections.Specialized;
+using System.Numerics;
 using Avalonia.Collections;
 using Avalonia.Data;
 using Avalonia.Logging;
@@ -457,11 +458,27 @@ namespace Avalonia
             => new CompositionDrawListVisual(compositor,
                 new ServerCompositionDrawListVisual(compositor.Server, this), this);
         
+        private Func<Vector3> _getScale = ()=> Vector3.One;
+        private Func<Vector3> _getCenterPoint = ()=> Vector3.One;
+        private Func<Vector3> _getOffset = ()=> Vector3.One;
+        private Func<float> _getOpacity = ()=> 1f;
+
+        public Vector3 CurrentCompositionScale => _getScale();
+        public Vector3 CurrentCompositionCenterPoint => _getCenterPoint();
+        public Vector3 CurrentOffset => _getOffset();
+        public float CurrentCompositionOpacity => _getOpacity();
+
         internal CompositionVisual AttachToCompositor(Compositor compositor)
         {
             if (CompositionVisual == null || CompositionVisual.Compositor != compositor)
             {
-                CompositionVisual = CreateCompositionVisual(compositor);
+                var serverCompositionDrawListVisual = new ServerCompositionDrawListVisual(compositor.Server, this);
+                CompositionVisual = new CompositionDrawListVisual(compositor,
+                    serverCompositionDrawListVisual, this);
+                _getScale = () => serverCompositionDrawListVisual.Scale;
+                _getOpacity = () => serverCompositionDrawListVisual.Opacity;
+                _getCenterPoint = () => serverCompositionDrawListVisual.CenterPoint;
+                _getOffset = () => serverCompositionDrawListVisual.Offset;
             }
 
             return CompositionVisual;
